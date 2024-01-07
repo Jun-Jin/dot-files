cc242dcbcb3c639f4ec9b99c59b771201057caf5
Jun Jin <shun.kin.sft@gmail.com>
2024-01-07 15:40:16 +0900

    config update

--- nvim/lua/rc/plugins/copilot.lua
+++ nvim/lua/rc/plugins/copilot.lua
@@ -1,3 +1,7 @@
+vim.g.copilot_filetypes = {
+  markdown = true,
+  yaml = true,
+}
 return {
   'github/copilot.vim',
   event = "InsertEnter",
@@ -7,8 +11,6 @@ return {
       -- https://github.com/orgs/community/discussions/29817#discussioncomment-4217615
       -- keymap.set("i", "<Tab>", 'copilot#')
       keymap.set("i", "<C-g>", 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false })
-      keymap.set("i", "<C-.>", "copilot#Next()", { silent = true, expr = true, replace_keycodes = false })
-      keymap.set("i", "<C-,>", "copilot#Previous()", { silent = true, expr = true, replace_keycodes = false })
       keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
   end,
 }
